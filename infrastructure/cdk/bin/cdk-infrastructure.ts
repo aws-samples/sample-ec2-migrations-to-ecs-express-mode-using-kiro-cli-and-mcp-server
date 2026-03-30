19b0950680a2e2bbf1ab28e3f27df24d387f655c
drosea <drosea@amazon.co.uk>
2026-03-30 09:57:43 +0100

    fixes for cdk and deploy scripts

--- infrastructure/cdk/bin/cdk-infrastructure.ts
+++ infrastructure/cdk/bin/cdk-infrastructure.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkInfrastructureStack } from '../lib/cdk-infrastructure-stack';
 
 const app = new cdk.App();
-new CdkInfrastructureStack(app, 'CdkInfrastructureStack', {
+new CdkInfrastructureStack(app, 'BlogAppStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
